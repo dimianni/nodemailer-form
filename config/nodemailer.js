f08dc816f58de8c625d84a9f7b976d6e5a1706a5
Dmytro Anikin <dimianni@users.noreply.github.com>
2023-04-12 23:11:46 +0200

    Zoho

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,8 +1,8 @@
 import nodemailer from 'nodemailer'
 
-const email = process.env.EMAIL
-const pass = process.env.EMAIL_PASS
-const host = process.env.HOST
+const email = process.env.EMAIL_ZOHO
+const pass = process.env.EMAIL_PASS_ZOHO
+const host = process.env.HOST_ZOHO
 
 // https://nodemailer.com/usage/
 // export const transporter = nodemailer.createTransport({
@@ -13,11 +13,22 @@ const host = process.env.HOST
 //     }
 // })
 
-// Sending to custom email using SMTP
+// Sending to custom email using SMTP (improvmx)
+// export const transporter = nodemailer.createTransport({
+//     host: host,
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//         user: email,
+//         pass: pass
+//     }
+// });
+
+// Sending to custom email using SMTP (zoho mail)
 export const transporter = nodemailer.createTransport({
     host: host,
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: email,
         pass: pass
