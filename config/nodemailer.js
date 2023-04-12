319c8c53e06233d48278e58c5b444258a84350ed
Dmytro Anikin <dimianni@users.noreply.github.com>
2023-04-12 19:58:34 +0200

    Sending to custom email

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,15 +2,27 @@ import nodemailer from 'nodemailer'
 
 const email = process.env.EMAIL
 const pass = process.env.EMAIL_PASS
+const host = process.env.HOST
 
 // https://nodemailer.com/usage/
+// export const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: email,
+//         pass
+//     }
+// })
+
+// Sending to custom email using SMTP
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: host,
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: email,
-        pass
+        pass: pass
     }
-})
+});
 
 export const mailOptions = {
     from: email,
